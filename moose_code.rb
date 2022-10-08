a1680cc2fbb590b5abeaaab751aae56594fa944a
Pierre <piercel.coder2022@gmail.com>
2022-10-08 18:28:29 +0200

    Modify the solution

--- moose_code.rb
+++ moose_code.rb
@@ -1,38 +1,32 @@
 # instance variable storing Morse code alphabets
-@moose_code_alphabet = {
-  '.-' => 'A',
-  '-...' => 'B',
-  '-.-.' => 'c',
-  '-..' => 'D',
-  '.' => 'E',
-  '..-.' => 'F',
-  '--.' => 'G',
-  '....' => 'H',
-  '..' => 'I',
-  '.---' => 'J',
-  '-.-' => 'K',
-  '.-..' => 'L',
-  '--' => 'M',
-  '-.' => 'N',
-  '---' => 'O',
-  '.--.' => 'P',
-  '--.-' => 'Q',
-  '.-.' => 'R',
-  '...' => 'S',
-  '-' => 'T',
-  '..-' => 'U',
-  '...-' => 'V',
-  '.--' => 'W',
-  '-..-' => 'X',
-  '-.--' => 'Y',
-  '--..' => 'Z'
-}
-
-# method to decode a Morse code character
-# Result is in uppercase
-def decode_char(letter)
-  @current_char = @moose_code_alphabet[letter].upcase
-  @current_char
+def decode_char(char)
+  morse_code = { '.-' => 'A',
+                 '-...' => 'B',
+                 '-.-.' => 'C',
+                 '-..' => 'D',
+                 '.' => 'E',
+                 '..-.' => 'F',
+                 '--.' => 'G',
+                 '....' => 'H',
+                 '..' => 'I',
+                 '.---' => 'J',
+                 '-.-' => 'K',
+                 '.-..' => 'L',
+                 '--' => 'M',
+                 '-.' => 'N',
+                 '---' => 'O',
+                 '.--.' => 'P',
+                 '--.-' => 'Q',
+                 '.-.' => 'R',
+                 '...' => 'S',
+                 '-' => 'T',
+                 '..-' => 'U',
+                 '...-' => 'V',
+                 '.--' => 'W',
+                 '-..-' => 'X',
+                 '-.--' => 'Y',
+                 '--..' => 'Z' }
+  morse_code[char]
 end
 
 # method to decode an entire word in Morse code
