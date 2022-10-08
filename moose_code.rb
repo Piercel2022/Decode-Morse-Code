990aee3deaeda81072c97d4785637661e8f69f83
Pierre <piercel.coder2022@gmail.com>
2022-10-08 09:54:08 +0200

    Add the code alphabet and the decode_char function

--- moose_code.rb
+++ moose_code.rb
@@ -34,21 +34,3 @@ def decode_char(letter)
   @current_char = @moose_code_alphabet[letter].upcase
   @current_char
 end
-
-# method to decode an entire word in Morse code
-def decode_word(word)
-  @letters = word.split
-  @current_word = ' '
-  @letters.each do |letter|
-    decode_char(letter)
-    @current_word += decode_char(letter)
-  end
-end
-
-# method to decode an entire message in Morse code
-def decode_message(message)
-  @words = message.split('   ')
-  @words.each do |word|
-    decode_word(word)
-  end
-end
